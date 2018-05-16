68197014b97245c81acb2aa10d6780858b75952e
Akinyi <margaretaoch@gmail.com>
2018-05-16 16:29:13 +0200

    Merge branch 'master' into Branch2
    # Conflicts:
    #	src/main/java/se/javagroup/projecttask/repository/UserRepository.java
    #	src/main/java/se/javagroup/projecttask/service/Service.java
    #	src/main/resources/application.properties
    #	target/classes/application.properties
    #	target/classes/se/javagroup/projecttask/repository/data/User.class
    #	target/classes/se/javagroup/projecttask/resource/UserResource.class
    #	target/classes/se/javagroup/projecttask/service/Service.class

---
+++ src/main/java/se/javagroup/projecttask/service/exception/TeamNotFoundException.java
@@ -0,0 +1,7 @@
+package se.javagroup.projecttask.service.exception;
+
+public class TeamNotFoundException extends RuntimeException {
+    public TeamNotFoundException(String message) {
+        super(message);
+    }
+}
