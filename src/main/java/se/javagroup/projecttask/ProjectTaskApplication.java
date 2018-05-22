a5ee098956a774b2f1d700e956c1da618c938a6e
Joel Swanelid <joel.swanelid@gmail.com>
2018-05-22 19:52:36 +0200

    second reformatering

--- src/main/java/se/javagroup/projecttask/ProjectTaskApplication.java
+++ src/main/java/se/javagroup/projecttask/ProjectTaskApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectTaskApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProjectTaskApplication.class, args);
     }
-
 }
